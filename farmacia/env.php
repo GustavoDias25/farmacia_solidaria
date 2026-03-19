3bcdf0c798c3daed7fe07afdd3ae0b856b30c3c6
GustavoDias25 <166569332+GustavoDias25@users.noreply.github.com>
2026-03-19 16:34:03 -0300

    Delete farmacia directory

--- farmacia/env.php
+++
@@ -1,6 +0,0 @@
-<?php
-$ENV_HOST = '127.0.0.1:3307';
-
-$ENV_USERNAME = 'root';
-$ENV_PASSWORD = '';
-$ENV_DBNAME = 'farmacia';
--- farmacia/nav-bar.php
+++
@@ -1,5 +0,0 @@
- <nav class="navbar navbar-fixed-top cabecalho">
-        <div class="container-fluid">
-            <h3 class="texto" style="font-size:1.7rem; color:white;">Farmácia</h3>
-        </div>
-    </nav>
